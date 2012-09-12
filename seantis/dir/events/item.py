c8684d316218252f63ec381ed639a72fe38581a9
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-09-12 10:45:24 +0200

    Adds basic fields to event item

--- seantis/dir/events/item.py
+++ seantis/dir/events/item.py
@@ -1,10 +1,11 @@
 from five import grok
-from zope.schema import TextLine
+from zope.schema import Text, TextLine, URI
 from collective.dexteritytextindexer import searchable
+from plone.namedfile.field import NamedImage, NamedFile
 
 from seantis.dir.base import item
 from seantis.dir.base import core
-from seantis.dir.base import utils
+from seantis.dir.base.schemafields import Email
 from seantis.dir.base.interfaces import IFieldMapExtender, IDirectoryItem
 
 from seantis.dir.events.directory import IEventsDirectory
@@ -13,7 +14,114 @@ from seantis.dir.events import _
 class IEventsDirectoryItem(IDirectoryItem):
     """Extends the seantis.dir.IDirectoryItem."""
 
-    pass
+    submitter = TextLine(
+        title=_(u'Submitter Name'),
+        required=False
+    )
+
+    submitter_email = Email(
+        title=_(u'Submitter Email'),
+        required=False,
+    )
+
+    searchable('short_description')
+    short_description = Text(
+        title=_(u'Short Description'),
+        required=True
+    )
+
+    searchable('long_description')
+    long_description = Text(
+        title=_(u'Long Description'),
+        required=False
+    )
+
+    image = NamedImage(
+        title=_(u'Opening Hours'),
+        required=False
+    )
+
+    attachment_1 = NamedFile(
+        title=_(u'Attachment 1'),
+        required=False
+    )
+
+    attachment_2 = NamedFile(
+        title=_(u'Attachment 2'),
+        required=False
+    )
+
+    searchable('locality')
+    locality = TextLine(
+        title=_(u'Locality'),
+        required=False
+    )
+
+    searchable('street')
+    street = TextLine(
+        title=_(u'Street'),
+        required=True
+    )
+
+    searchable('housenumber')
+    housenumber = TextLine(
+        title=_(u'Housenumber'),
+        required=False
+    )
+
+    searchable('zipcode')
+    zipcode = TextLine(
+        title=_(u'Zipcode'),
+        required=False
+    )
+
+    searchable('town')
+    town = TextLine(
+        title=_(u'Town'),
+        required=True
+    )
+
+    searchable('website')
+    website = URI(
+        title=_(u'Website'),
+        required=False
+    )
+
+    searchable('organizer')
+    organizer = TextLine(
+        title=_(u'Organizer'),
+        required=True
+    )
+
+    searchable('contact_name')
+    contact_name = TextLine(
+        title=_(u'Contact Name'),
+        required=False
+    )
+
+    searchable('contact_email')
+    contact_email = Email(
+        title=_(u'Contact Email'),
+        required=True
+    )
+
+    searchable('contact_phone')
+    contact_phone = TextLine(
+        title=_(u'Contact Phone'),
+        required=False
+    )
+
+    searchable('prices')
+    prices = Text(
+        title=_(u'Prices'),
+        required=False
+    )
+
+    searchable('registration')
+    registration = URI(
+        title=_(u'Tickets / Registration'),
+        required=False
+    )
 
 class EventsDirectoryItem(item.DirectoryItem):
     pass
