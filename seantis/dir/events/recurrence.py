b69817f7f70ed26375ec973eebf71096ca843163
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-10-05 11:17:01 +0200

    Adds date url to occurrences

--- seantis/dir/events/recurrence.py
+++ seantis/dir/events/recurrence.py
@@ -1,5 +1,6 @@
 from zope.proxy import ProxyBase
 from dateutil.rrule import rrulestr
+from urllib import urlencode
 
 from seantis.dir.events.utils import overlaps, to_utc
 
@@ -8,6 +9,12 @@ from seantis.dir.events.utils import overlaps, to_utc
 # my title/description attributes ore not showing my methods).
 
 # This pure proxy has none of those problems. 
+
+# ========================================================================
+# THERE IS ONE CAVEAT. In templates "context/start" will yield the wrapped
+# item's start. "python: context.start" on the other hand will yield the
+# occurence's start. Same with other properties/methods
+# ========================================================================
 class Occurrence(ProxyBase):
 
     __slots__ = ('_wrapped', '_start', '_end')
@@ -27,6 +34,26 @@ class Occurrence(ProxyBase):
     @property
     def end(self):
         return self._end
+
+    def url(self):
+        """ Adds the date of the occurrence to the result of absolute_url. This
+        allows to distinguish between occurrences.
+
+        There would be two other options, but they are less ideal:
+
+        1. Add the whole date-time string - Results in a less than pretty url.
+        2. Add an occurrence count - The underlying item of the url might 
+        change without any chance to tell the user.
+
+        """
+        base = self._wrapped.absolute_url()
+        
+        # don't add anything if the wrapped item doesn't have recurrence
+        if not self._wrapped.recurrence:
+            return base
+
+        base += '?' in base and '&' or '?'
+        return base + urlencode({'date': self._start.strftime('%y-%m-%d')})
 
 def occurrences(item, min_date, max_date):
 
