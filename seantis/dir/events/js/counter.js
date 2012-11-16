1163c96ad097f43228ffcd5433a3b995671d3ee3
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-11-16 10:41:32 +0100

    Completes javascript counter

--- seantis/dir/events/js/counter.js
+++ seantis/dir/events/js/counter.js
@@ -1,5 +1,16 @@
 var inputtext_countdown = function($, input, label, template, maxlength) {
-    
+    /*  Shows an input text countdown in a label and ensures that the
+    max length is not overstepped.
+
+    @$          instance of jQuery
+    @input      jQuery instance or string selector for the input field
+    @label      jQuery instance or string selector for the label where
+                the countdown will be shown
+    @template   jQuery template containing ${chars} where the count should
+                be rendered
+    @maxlength  the maximum length that should be enforced
+    */
+
     var $input = $(input);
     var $label = $(label);
     var initial_text = $label.text();
@@ -18,16 +29,34 @@ var inputtext_countdown = function($, input, label, template, maxlength) {
         }
     };
 
-    $input.keypress(function() {
+    var update = function(e) {
+        var length = getlength();
+        
+        if (length > maxlength) {
+            $input.val($input.val().substring(0, maxlength));
+        }
+        
         $label.text(gettext(getlength()));
-    });
-}
+    };
+
+    update();
+
+    var events = ['keydown', 'paste', 'cut'];
+    for (var i=0; i<events.length; i++) {
+        $input.bind(events[0], function() {
+            setTimeout(update, 25);
+        });
+    }
+
+    $input.bind('keydown', update);
+    $input.bind('paste', update);
+    $input.bind('cut', update);
+};
 
 jQuery(document).ready(function() {
     inputtext_countdown($,
         '#formfield-form-widgets-short_description textarea',
         '#formfield-form-widgets-short_description span.formHelp',
-        '${chars} Zeichen verbleibend',
-        140
-    ); 
+        $('.event-submit-form').attr('data-countdown-template'),
+        140);
 });
