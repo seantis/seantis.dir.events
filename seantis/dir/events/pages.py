a1f1928ec5e2bccdebf5d8a162c5e7a3ffb1dbff
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-11-21 17:18:04 +0100

    Adds ability to define a custom css by page id

--- seantis/dir/events/pages.py
+++ seantis/dir/events/pages.py
@@ -3,11 +3,19 @@ import re, json
 import logging
 log = logging.getLogger('seantis.dir.events')
 
+from five import grok
+
 from zope.component.hooks import getSite
 from zope.publisher.interfaces import IPublishTraverse
 from zope.interface import implements
 from ZPublisher.BaseRequest import DefaultPublishTraverse
 
+from plone.app.layout.viewlets.interfaces import IHtmlHeadLinks
+
+from zope.cachedescriptors import property as cacheproperty
+
+from seantis.dir.base.directory import DirectoryCatalogMixin
+from seantis.dir.base.interfaces import IDirectoryRoot
 from seantis.dir.base.session import get_session, set_session
 
 key = 'seantis-events-pageid'
@@ -18,6 +26,9 @@ def get_custom_pageid():
 
 def set_custom_pageid(pageid):
     set_session(getSite(), key, pageid)
+
+def clear_custom_pageid():
+    set_session(getSite(), key, None)
 
 def custom_properties(directory, pageid=None):
     pageid = pageid or get_custom_pageid()
@@ -48,6 +59,8 @@ class CustomPageMixin(object):
         
         match = re.match(pattern, name)
         if match:
+            request._match_request = True
+
             pageid = match.group(1)
             
             log.debug('setting pageid to %s' % pageid)
@@ -55,4 +68,26 @@ class CustomPageMixin(object):
 
             return self
 
+        if not hasattr(request, '_match_request') and name == 'view' and not request.get_header('referer'):
+            clear_custom_pageid()
+
         return DefaultPublishTraverse(self, request).publishTraverse(request, name)
+
+class CustomPageViewlet(grok.Viewlet, DirectoryCatalogMixin):
+    grok.context(IDirectoryRoot)
+    grok.name('seantis.dir.events.custom')
+    grok.require('zope2.View')
+    grok.viewletmanager(IHtmlHeadLinks)
+    grok.order(99) # as far below as possible
+
+    template = grok.PageTemplateFile('templates/custom.pt')
+
+    @cacheproperty.cachedIn('_custom')
+    def custom(self):
+        return custom_properties(self.directory) or {}
+
+    def custom_css(self):
+        return self.custom.get('css', '')
+
+    def custom_script(self):
+        return self.custom.get('script', '')
