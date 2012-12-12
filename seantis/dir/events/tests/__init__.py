d8ca680486dd62a5456261e1ccb5288c632290c1
Tobias Reinhard <tobias.reinhard@seantis.ch>
2012-12-12 11:07:59 +0100

    Code cleanup.

--- seantis/dir/events/tests/__init__.py
+++ seantis/dir/events/tests/__init__.py
@@ -8,15 +8,18 @@ from AccessControl import getSecurityManager
 from plone.testing import z2
 from plone.dexterity.utils import createContentInContainer
 from plone.app import testing
+from plone.testing.z2 import Browser
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore import permissions
 
 from zope.component.hooks import getSite
 
-from seantis.dir.events.tests.layer import INTEGRATION_TESTING, FUNCTIONAL_TESTING
+from seantis.dir.events.tests.layer import INTEGRATION_TESTING
+from seantis.dir.events.tests.layer import FUNCTIONAL_TESTING
+
 
 class IntegrationTestCase(unittest.TestCase):
-    
+
     layer = INTEGRATION_TESTING
 
     def setUp(self):
@@ -44,7 +47,7 @@ class IntegrationTestCase(unittest.TestCase):
         testing.login(self.portal, 'test-user')
 
     def logout(self):
-        testing.logout()   
+        testing.logout()
 
     @property
     def workflow(self):
@@ -55,13 +58,13 @@ class IntegrationTestCase(unittest.TestCase):
 
     def create_event(self, **kw):
         """ Create an event in self.directory. By default, test-user must
-        be logged in or the creation will be unauthorized. 
+        be logged in or the creation will be unauthorized.
 
         """
 
         defaults = {
             'start': datetime.today(),
-            'end': datetime.today() + timedelta(seconds=60*60),
+            'end': datetime.today() + timedelta(seconds=60 * 60),
             'timezone': 'Europe/Zurich',
             'recurrence': ''
         }
@@ -73,14 +76,13 @@ class IntegrationTestCase(unittest.TestCase):
         return createContentInContainer(
             self.directory, 'seantis.dir.events.item', **kw
         )
-        
+
     def has_permission(self, obj, permission):
         return bool(getSecurityManager().checkPermission(permission, obj))
 
     def may_view(self, obj):
         return self.has_permission(obj, permissions.View)
 
-from plone.testing.z2 import Browser
 
 class BetterBrowser(Browser):
 
@@ -100,7 +102,7 @@ class BetterBrowser(Browser):
         assert 'logged out' in self.contents
 
     def login_admin(self):
-        self.login('admin', 'secret') 
+        self.login('admin', 'secret')
 
     def login_testuser(self):
         self.login('test-user', 'secret')
@@ -114,10 +116,10 @@ class BetterBrowser(Browser):
             self.open(url)
         except Exception, e:
 
-            # zope does not always raise unathorized exceptions with the correct
-            # class signature, so we need to do this thing:
+            # zope does not always raise unathorized exceptions with the
+            # correct class signature, so we need to do this thing:
             expected = e.__repr__().startswith(exception)
-            
+
             if not expected:
                 raise
 
@@ -131,12 +133,13 @@ class BetterBrowser(Browser):
 
     def show_in_browser(self):
         """ Opens the current contents in the default system browser """
-        tempfile = NamedTemporaryFile(delete=False) 
+        tempfile = NamedTemporaryFile(delete=False)
         tempfile.write(self.contents)
         tempfile.close()
 
-        os.rename(tempfile.name, tempfile.name+'.html')
-        os.system("open "+tempfile.name+'.html');
+        os.rename(tempfile.name, tempfile.name + '.html')
+        os.system("open " + tempfile.name + '.html')
+
 
 class FunctionalTestCase(IntegrationTestCase):
 
@@ -145,7 +148,7 @@ class FunctionalTestCase(IntegrationTestCase):
     def setUp(self):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
-    
+
     def new_browser(self):
         browser = BetterBrowser(self.app)
         browser.portal = self.portal
