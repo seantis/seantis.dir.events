1c94e790cdff2e89af7eb826fa8868dfd1f132a4
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-02-11 11:04:47 +0100

    Ensures that no events are shown from the past

--- seantis/dir/events/catalog.py
+++ seantis/dir/events/catalog.py
@@ -9,7 +9,10 @@ from seantis.dir.base.interfaces import IDirectoryCatalog
 
 from seantis.dir.events import dates
 from seantis.dir.events import recurrence
-from seantis.dir.events.interfaces import IEventsDirectory, IEventsDirectoryItem
+from seantis.dir.events.interfaces import (
+    IEventsDirectory, IEventsDirectoryItem
+)
+
 
 class EventsDirectoryCatalog(DirectoryCatalog):
 
@@ -24,11 +27,11 @@ class EventsDirectoryCatalog(DirectoryCatalog):
     @property
     def submitted_count(self):
         """ Returns the submitted count depending on the current date filter
-        but independent of the current states filter. 
+        but independent of the current states filter.
 
         This needs to loop through all elements again so use only if needed.
         """
-        
+
         results = self.catalog(path={'query': self.path, 'depth': 1},
             object_provides=IEventsDirectoryItem.__identifier__,
             review_state=('submitted', )
@@ -46,30 +49,30 @@ class EventsDirectoryCatalog(DirectoryCatalog):
 
         return submitted_count
 
-    @property
-    def daterange(self):
+    def get_daterange(self):
         return self._daterange
 
-    @daterange.setter
     @instance.clearafter
-    def daterange(self, range):
+    def set_daterange(self, range):
         assert dates.is_valid_daterange(range), "invalid date range %s" % range
         self._daterange = range
 
-    @property
-    def states(self):
+    daterange = property(get_daterange, set_daterange)
+
+    def get_states(self):
         return self._states
 
-    @states.setter
     @instance.clearafter
-    def states(self, states):
+    def set_states(self, states):
         for state in states:
             # as an added security measure it is not yet possible to
             # query for previewed events as they should only be availble
             # to the user with the right token (see form.py)
             assert state in ('submitted', 'published', 'archived')
-        
+
         self._states = states
+
+    states = property(get_states, set_states)
 
     def sortkey(self):
         return lambda i: i.start
@@ -89,11 +92,12 @@ class EventsDirectoryCatalog(DirectoryCatalog):
         for item in realitems:
             for occurrence in recurrence.occurrences(item, start, end):
                 for split in recurrence.split_days(occurrence):
-                    yield split
+                    if dates.overlaps(start, end, split.start, split.end):
+                        yield split
 
     def hide_blocked(self, realitems):
         """ Returns a generator filtering real items, with the submitted items
-        which the user can see but not publish left out. 
+        which the user can see but not publish left out.
 
         As this could be reeealy slow we do a bit of a shortcut by only
         checking for the publish action right on the item. The guard is
@@ -118,7 +122,7 @@ class EventsDirectoryCatalog(DirectoryCatalog):
 
         if len(nonempty_terms) == 0:
             return self.items()
-        
+
         real = super(EventsDirectoryCatalog, self).filter(term)
         return sorted(self.spawn(self.hide_blocked(real)), key=self.sortkey())
 
