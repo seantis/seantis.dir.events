09cb650ea051d5a5cca4cdc10921710bf1547705
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-07-10 16:42:32 +0200

    Remove time javascript and add a simple time input fixing routine

--- seantis/dir/events/js/form.js
+++ seantis/dir/events/js/form.js
@@ -60,13 +60,75 @@
         enable_fields(time_fields, enable_time_fields());
     };
 
+    var pad = function(num, size) {
+        var s = "000000000" + num;
+        return s.substr(s.length-size);
+    };
+
+    var force_valid_time = function(time) {
+
+        var normalize = function(time) {
+            var digits = time.match(/\d+/g);
+
+            if (digits === null) {
+                return '';
+            }
+
+            digits = digits.join('').substring(0, 4);
+
+            switch(digits.length) {
+                case 0:
+                    return '';
+                case 1:
+                    return '0' + digits + ':00';
+                case 2:
+                    return digits + ':00';
+                case 3:
+                    return [
+                        digits.substring(0, 2),
+                        digits.substring(2, 3) + '0'
+                    ].join(':');
+                default:
+                    return [
+                        digits.substring(0, 2),
+                        digits.substring(2, 4)
+                    ].join(':');
+            }
+        };
+
+        var limit = function(time) {
+            if (time.length === 0) {
+                return '';
+            }
+
+            var components = time.split(':');
+            var hour = parseInt(components[0], 10),
+                minute = parseInt(components[1], 10);
+
+            hour = hour > 23 ? 23 : hour;
+            minute = minute > 59 ? 59 : minute;
+
+            return pad(hour, 2) + ':' + pad(minute, 2);
+        };
+
+        return limit(normalize(time));
+    };
+
+    var fix_time_inputs = function(e) {
+        var input = $(this);
+        input.val(force_valid_time(input.val()));
+    };
+
     $(document).ready(function() {
         $(submission_input).change(update_submission_fields);
         $(wholeday_field).change(update_submission_fields);
         update_submission_fields();
 
-        $(time_fields.join(', ')).timePicker({
-            separator: ':'
-        });
+        for (var i=0; i < time_fields.length; i++) {
+            var field = $(time_fields[i]);
+
+            field.attr('placeholder', 'hh:mm');
+            field.change(fix_time_inputs);
+        }
     });
 })(jQuery);
